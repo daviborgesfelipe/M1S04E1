89b750ad9bff2fbc9488b22691233413308682e9
Davi Borges <davifelipe_300@hotmail.com>
2022-03-23 20:20:59 -0300

    Operador Logico Maior que, Menor que, If Else

---
+++ index.js
@@ -0,0 +1,10 @@
+let idade = 20;
+if (idade > 65){
+    console.log(`Você é Idoso`)
+}
+else if(idade <= 64){
+    console.log(`Você é Adulto`)
+}
+if (idade <= 16){
+    console.log(`Você é jovem`)
+}
